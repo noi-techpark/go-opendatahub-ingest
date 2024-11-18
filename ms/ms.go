dc9e71873b38898404d50461f0a82493a18d8481
Clemens Zagler <c.zagler@noi.bz.it>
2024-11-18 16:04:17 +0100

    removed single Init function because CBA with figuring out the generics to make it work right now

--- ms/ms.go
+++ ms/ms.go
@@ -7,8 +7,6 @@ package ms
 import (
 	"log/slog"
 	"os"
-
-	"github.com/kelseyhightower/envconfig"
 )
 
 func FailOnError(err error, msg string) {
@@ -28,23 +26,7 @@ func InitLog(lv string) {
 
 // Default configuration parameters for logging and provider that all data collectors/transformers need
 // Embed this into your env struct
-type BaseEnv struct {
+type Env struct {
 	PROVIDER string
 	LOGLEVEL string `default:"INFO"`
-}
-
-func (b *BaseEnv) LogLevel() string {
-	return b.LOGLEVEL
-}
-
-type Env interface {
-	BaseEnv
-	LogLevel() string
-}
-
-// Convenience one shot setup method
-// Create config from env and initialize logging system
-func Init[E Env](e *E) {
-	envconfig.MustProcess("", e)
-	InitLog((*e).LogLevel())
 }
